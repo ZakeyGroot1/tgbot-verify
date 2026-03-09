f87bec155bdb23b2b2ac1d34fdebc101be514a53
ZakeyGroot1 <zakeyusa@gmail.com>
2026-03-09 11:48:04 +0500

    Create keep_alive.py

---
+++ keep_alive.py
@@ -0,0 +1,17 @@
+from flask import Flask
+from threading import Thread
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "I am alive and running!"
+
+def run():
+    # Render assigns a random port, but defaults to 10000. 
+    # 0.0.0.0 makes it accessible to the outside world.
+    app.run(host='0.0.0.0', port=10000)
+
+def keep_alive():
+    t = Thread(target=run)
+    t.start()
